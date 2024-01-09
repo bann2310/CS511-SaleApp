a3c925a80c4de7c1616fd788d167208b31e55602
Nguyen Bao <21520618@gm.uit.edu.vn>
2024-01-09 16:24:55 +0700

    Add productdetail

---
+++ Backend/1015bookstore.window/MainPage/Products/ProductDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1015bookstore.window.MainPage.Products
+{
+    public partial class ProductDetails : UserControl
+    {
+        public ProductDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
