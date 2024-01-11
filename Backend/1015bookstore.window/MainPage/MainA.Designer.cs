acb10669484c7f9cda4a464808258c981ae2d6b7
Nguyễn Minh Duy <91718109+bann2310@users.noreply.github.com>
2024-01-11 10:15:56 +0700

    Update MainA.Designer.cs

--- Backend/1015bookstore.window/MainPage/MainA.Designer.cs
+++ Backend/1015bookstore.window/MainPage/MainA.Designer.cs
@@ -73,7 +73,7 @@
             this.metroPanel1.Location = new System.Drawing.Point(0, 0);
             this.metroPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.metroPanel1.Name = "metroPanel1";
-            this.metroPanel1.Size = new System.Drawing.Size(1828, 80);
+            this.metroPanel1.Size = new System.Drawing.Size(1462, 75);
             this.metroPanel1.TabIndex = 1;
             this.metroPanel1.VerticalScrollbarBarColor = true;
             this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
@@ -84,7 +84,7 @@
             this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(1642, 18);
+            this.pictureBox4.Location = new System.Drawing.Point(1322, 16);
             this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(40, 40);
@@ -99,7 +99,7 @@
             this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(1559, 18);
+            this.pictureBox3.Location = new System.Drawing.Point(1243, 16);
             this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(40, 40);
@@ -118,7 +118,7 @@
             this.panel1.Controls.Add(this.iconPictureBox1);
             this.panel1.Controls.Add(this.search_content);
             this.panel1.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.panel1.Location = new System.Drawing.Point(550, 18);
+            this.panel1.Location = new System.Drawing.Point(440, 17);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(650, 40);
@@ -130,10 +130,10 @@
             this.label_u.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.label_u.Font = new System.Drawing.Font("Roboto", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_u.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
-            this.label_u.Location = new System.Drawing.Point(50, 8);
+            this.label_u.Location = new System.Drawing.Point(52, 9);
             this.label_u.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_u.Name = "label_u";
-            this.label_u.Size = new System.Drawing.Size(187, 20);
+            this.label_u.Size = new System.Drawing.Size(182, 20);
             this.label_u.TabIndex = 16;
             this.label_u.Text = "Sách bao rẻ, rất rẻ áaaa";
             this.label_u.Click += new System.EventHandler(this.Typing_Click);
@@ -146,10 +146,10 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Roboto", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(520, 2);
+           this.button1.Location = new System.Drawing.Point(517, 4);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 30);
+            this.button1.Size = new System.Drawing.Size(123, 30);
             this.button1.TabIndex = 2;
             this.button1.Text = "Tìm kiếm";
             this.button1.UseVisualStyleBackColor = false;
@@ -164,7 +164,7 @@
             this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
             this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconPictureBox1.IconSize = 36;
-            this.iconPictureBox1.Location = new System.Drawing.Point(10, 1);
+            this.iconPictureBox1.Location = new System.Drawing.Point(10, 3);
             this.iconPictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.iconPictureBox1.Name = "iconPictureBox1";
             this.iconPictureBox1.Size = new System.Drawing.Size(36, 36);
@@ -178,10 +178,10 @@
             this.search_content.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.search_content.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.search_content.ForeColor = System.Drawing.Color.Black;
-            this.search_content.Location = new System.Drawing.Point(48, 8);
+            this.search_content.Location = new System.Drawing.Point(50, 9);
             this.search_content.Margin = new System.Windows.Forms.Padding(2);
             this.search_content.Name = "search_content";
-            this.search_content.Size = new System.Drawing.Size(468, 25);
+            this.search_content.Size = new System.Drawing.Size(464, 20);
             this.search_content.TabIndex = 0;
             this.search_content.TabStop = false;
             this.search_content.Click += new System.EventHandler(this.Typing_Click);
@@ -192,7 +192,7 @@
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(25, 25);
+             this.pictureBox2.Location = new System.Drawing.Point(20, 20);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(40, 40);
@@ -209,7 +209,7 @@
             this.close.IconColor = System.Drawing.SystemColors.ControlText;
             this.close.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.close.IconSize = 40;
-            this.close.Location = new System.Drawing.Point(1758, 16);
+            this.close.Location = new System.Drawing.Point(1406, 13);
             this.close.Margin = new System.Windows.Forms.Padding(2);
             this.close.Name = "close";
             this.close.Size = new System.Drawing.Size(40, 40);
@@ -223,11 +223,11 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label2.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label2.Location = new System.Drawing.Point(200, 41);
+            this.label2.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(170, 36);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(155, 37);
+            this.label2.Size = new System.Drawing.Size(139, 33);
             this.label2.TabIndex = 2;
             this.label2.Text = "Bookstore";
             // 
@@ -236,11 +236,11 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label1.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(200, 12);
+            this.label1.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(170, 10);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 37);
+            this.label1.Size = new System.Drawing.Size(75, 33);
             this.label1.TabIndex = 1;
             this.label1.Text = "1015";
             // 
@@ -249,10 +249,10 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(145, 10);
+            this.pictureBox1.Location = new System.Drawing.Point(116, 7);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(51, 60);
+            this.pictureBox1.Size = new System.Drawing.Size(52, 60);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -265,7 +265,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Roboto", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(1604, 15);
+             this.button2.Location = new System.Drawing.Point(1247, 15);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(116, 46);
@@ -280,10 +280,10 @@
             this.body.AutoScroll = true;
             this.body.BackColor = System.Drawing.Color.WhiteSmoke;
             this.body.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.body.Location = new System.Drawing.Point(0, 80);
+            this.body.Location = new System.Drawing.Point(0, 75);
             this.body.Margin = new System.Windows.Forms.Padding(2);
             this.body.Name = "body";
-            this.body.Size = new System.Drawing.Size(1828, 1020);
+            this.body.Size = new System.Drawing.Size(1462, 927);
             this.body.TabIndex = 2;
             // 
             // MainA
